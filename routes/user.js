3397573a8692fe7b0c8e49a8047fb8ec5bea7fe1
nexium.Webworks <nexium.webworks@gmail.com>
2024-11-19 12:46:53 +0530

    userExist check added

--- routes/user.js
+++ routes/user.js
@@ -4,17 +4,28 @@ const userMiddleware = require("../middleware/user");
 const { User } = require("../db");
 const { default: mongoose } = require("mongoose");
 
-router.post('/signup', (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.create({
-        username, 
-        password
-    })
+router.post('/signup', async (req, res) => {
+    try {
+        const { username, password } = req.body;
 
-    res.json({
-        message: "User created successfully"
-    })
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(409).json({ message: "Username already exists. Please choose a different username." });
+        }
+
+        const newUser = await User.create({ username, password });
+
+        res.status(201).json({
+            message: "User created successfully",
+            user: {
+                id: newUser._id,
+                username: newUser.username,
+            },
+        });
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ message: "An error occurred while creating the user." });
+    }
 });
 
 router.post('/signin', (req, res) => {
